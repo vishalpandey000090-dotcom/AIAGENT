af53a16a0b90f1a11412f5be4589c6f6a16047f7
Bubble Wrap <vishalpandey000090@gmail.com>
2025-11-20 00:26:14 +0530

    your message

--- backend/frontend/src/screens/home.jsx
+++ backend/frontend/src/screens/home.jsx
@@ -1,14 +1,10 @@
-import React, { useContext } from 'react';
-import { UserContext } from '../context/user.context';
+import React, { useContext } from "react";
+import { UserContext } from "../context/user.context";
 
 const Home = () => {
   const { user } = useContext(UserContext);
 
-  return (
-    <div>
-      {JSON.stringify(user)}
-    </div>
-  );
+  return <div>{JSON.stringify(user)}</div>;
 };
 
 export default Home;
